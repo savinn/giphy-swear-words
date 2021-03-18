c5e7b8901097cee626701547e04115d3de70936b
savinn <savinn85@gmail.com>
2021-03-18 01:46:41 +0100

    naming

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Fedex'),
+      dir: require('path').join(__dirname, './coverage/giphy-swear-words'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
